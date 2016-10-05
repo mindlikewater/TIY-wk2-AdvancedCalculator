cc4c82bbd8ff70d52eefa79a1c1c46b1aa931512
Tai Chen <mindlikewater@gmail.com>
2016-10-04 22:17:20 -0400

    Built the basic display of the calculator

---
+++ main.js
@@ -0,0 +1,18 @@
+#clear {
+  background-color: rgb(238,91, 0);
+  border: 1px solid rgb(0, 69, 133);
+  padding: 10px;
+}
+
+// var calcButton = document.getElementById("calculate-button");
+//
+// var runSum = function () {
+//   var num1 = Number(document.getElementById("value-one").value);
+//   var num2 = Number(document.getElementById("value-two").value);
+//   var answerBox = document.getElementById("answer");
+//
+//   answerBox.innerHTML = "";
+//   answerBox.innerHTML += num1 + num2;
+// };
+//
+// calcButton.addEventListener("click", runSum);
